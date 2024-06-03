f4fac0d5dc66d48f25e0c78a8107ffb656934f98
Muhaned Yahya <105832361+MuhanedYahya@users.noreply.github.com>
2024-06-03 21:59:28 +0300

    automate_script_creation.sh modified

--- automate_script_creation.sh
+++ automate_script_creation.sh
@@ -11,7 +11,7 @@ script_path="$HOME/.local/bin/$script_name"
 
 # Create the script file in .local/bin/
 echo -e "#!/bin/bash\n\n$(cat "$script_name")" > "$script_path"
-
+rm $script_name
 # Set permissions for the script
 chmod +x "$script_path"
 
