f75b7b3920d39bc7691ffabb00fcb057a7a46379
Muhaned Yahya <105832361+MuhanedYahya@users.noreply.github.com>
2024-06-03 21:30:28 +0300

    automate_script_creation.sh Created

---
+++ automate_script_creation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the name of the script from the user
+read -p "Enter the name of the script: " script_name
+
+# Launch vim to allow the user to write the script
+vim "$script_name"
+
+# Set the file path for the script in .local/bin/
+script_path="$HOME/.local/bin/$script_name"
+
+# Create the script file in .local/bin/
+echo -e "#!/bin/bash\n\n$(cat "$script_name")" > "$script_path"
+
+# Set permissions for the script
+chmod +x "$script_path"
+
+echo "Script $script_name has been created and added to ~/.local/bin/ with appropriate permissions."
